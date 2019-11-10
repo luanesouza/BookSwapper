1f22d9809475078c1e8131df216f4815bb106258
luanesouza <luanesouza411@gmail.com>
2019-11-10 13:14:50 -0500

    adds css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
+import MyWishlist from './components/MyWishlist';
 
 class App extends Component {
-  return (
-    <main className="App">
 
-    </main>
-  );
+  render(){
+
+    return (
+      <main className="App">
+        <MyWishlist />
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/MyLibrary.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyLibrary = () => {
+  return ( <h1> My Books </h1>)
+}
+
+export default MyLibrary;
