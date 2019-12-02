432edaedc58ef9d577d93c0e4697d433acc55fe8
luanesouza <luanesouza411@gmail.com>
2019-12-02 15:08:40 -0500

    adds form

--- src/components/Login/_form.js
+++ src/components/Login/_form.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Form(){
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitForm = (evt) => {
+    evt.preventDefault()
+    console.log(evt.target.value);
+  }
+
   return(
     <section>
       <h2>Sup</h2>
 
-      <form>
+      <form onSubmit={(evt) => submitForm(evt)}>
+        <input name='username' value='' placeholder='username' onChange={(e) => setUsername(e.target.value)}/>
+        <input name='' value='' placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
+        <input name='' value='' placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
+        <button> Login </button>
       </form>
     </section>
   )
